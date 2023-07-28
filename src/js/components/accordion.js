f5a0d4d97741beb8cdfd6438d4d876b2b4f00059
Andrey Nazhiganov <b3rt@narod.ru>
2023-07-29 00:06:34 +0300

    Add footer

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,4 +1,5 @@
 let acc = null
+let footerAcc = null
 
 function checkResolution () {
   const screenWidth = window.innerWidth
@@ -6,11 +7,16 @@ function checkResolution () {
     if (!acc) {
       acc = new Accordion('.js-nav-accordion')
       acc.closeAll()
+
+      footerAcc = new Accordion('.js-footer-accordion')
+      footerAcc.closeAll()
     }
   } else {
     if (acc) {
       acc.destroy()
       acc = null
+      footerAcc.destroy()
+      footerAcc = null
     }
   }
 }
