5d08815c679fb28cd42d56b53972027f5315ee1e
Andrey Nazhiganov <b3rt@narod.ru>
2023-07-29 20:35:22 +0300

    Add menu backdrop

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,11 +1,16 @@
 const burger = document.querySelector('.js-burger')
 const menu = document.querySelector('.js-menu')
+const backdrop = document.querySelector('.js-backdrop')
 const MENU_ACTIVE_CLASS = 'menu--active'
 const BURGER_ACTIVE_CLASS = 'burger-btn--active'
+const BACKDROP_ACTIVE_CLASS = 'page__backdrop--active'
+const NO_SCROLL_CLASS = 'no-scroll'
 
 if (burger && menu) {
   burger.addEventListener('click', () => {
     menu.classList.toggle(MENU_ACTIVE_CLASS)
     burger.classList.toggle(BURGER_ACTIVE_CLASS)
+    backdrop.classList.toggle(BACKDROP_ACTIVE_CLASS)
+    document.body.classList.toggle(NO_SCROLL_CLASS)
   })
 }
