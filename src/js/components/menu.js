90808dcd73555ad21f30ce6789fab77c288b5376
Andrey Nazhiganov <b3rt@narod.ru>
2023-07-29 23:08:38 +0300

    Fix header menu closing & menu backdrop + footer pointer events

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -6,11 +6,33 @@ const BURGER_ACTIVE_CLASS = 'burger-btn--active'
 const BACKDROP_ACTIVE_CLASS = 'page__backdrop--active'
 const NO_SCROLL_CLASS = 'no-scroll'
 
+function closeMenu () {
+  menu.classList.remove(MENU_ACTIVE_CLASS)
+  burger.classList.remove(BURGER_ACTIVE_CLASS)
+  backdrop.classList.remove(BACKDROP_ACTIVE_CLASS)
+  document.body.classList.remove(NO_SCROLL_CLASS)
+}
+
+function openMenu () {
+  menu.classList.add(MENU_ACTIVE_CLASS)
+  burger.classList.add(BURGER_ACTIVE_CLASS)
+  backdrop.classList.add(BACKDROP_ACTIVE_CLASS)
+  document.body.classList.add(NO_SCROLL_CLASS)
+}
+
 if (burger && menu) {
   burger.addEventListener('click', () => {
-    menu.classList.toggle(MENU_ACTIVE_CLASS)
-    burger.classList.toggle(BURGER_ACTIVE_CLASS)
-    backdrop.classList.toggle(BACKDROP_ACTIVE_CLASS)
-    document.body.classList.toggle(NO_SCROLL_CLASS)
+    if (burger.classList.contains(BURGER_ACTIVE_CLASS)) {
+      closeMenu()
+    } else {
+      openMenu()
+    }
+  })
+
+  backdrop.addEventListener('click', closeMenu)
+  window.addEventListener('keydown', evt => {
+    if (evt.key === 'Escape') {
+      closeMenu()
+    }
   })
 }
