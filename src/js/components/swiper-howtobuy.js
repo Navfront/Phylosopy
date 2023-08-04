8dc67888b4e7339dd69194b7f9f3328aefc648ad
Andrey Nazhiganov <b3rt@narod.ru>
2023-08-04 23:03:14 +0300

    Add howtobuy section

--- src/js/components/swiper-howtobuy.js
+++ src/js/components/swiper-howtobuy.js
@@ -5,18 +5,26 @@ const SWIPER_SLIDE_CLASS = 'swiper-slide'
 
 let swiper = null
 
-function sliderInit () {
+function sliderInit (offset = 0) {
+  console.log('init')
   howtobuySlider.classList.add(SWIPER_CLASS)
   howtobuySlider.children[0].classList.add(SWIPER_WRAPPER_CLASS)
   Array.from(howtobuySlider.children[0].children).forEach(slideEl => {
     slideEl.classList.add(SWIPER_SLIDE_CLASS)
   })
   swiper = new Swiper(howtobuySlider, {
-    slidesPerView: '3'
+    slidesPerView: 'auto',
+
+    slidesOffsetBefore: offset,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
   })
 }
 
 function sliderDestroy () {
+  console.log('destr')
   howtobuySlider.classList.remove(SWIPER_CLASS)
   howtobuySlider.children[0].classList.remove(SWIPER_WRAPPER_CLASS)
   Array.from(howtobuySlider.children[0].children).forEach(slideEl => {
@@ -29,7 +37,7 @@ function sliderDestroy () {
 function howtobuySliderHandler () {
   const screenWidth = window.innerWidth
   if (screenWidth <= 1280 && screenWidth >= 695) {
-    if (!swiper) sliderInit()
+    if (!swiper) sliderInit(screenWidth <= 800 ? 0 : 80)
   } else if (swiper) {
     sliderDestroy()
   }
